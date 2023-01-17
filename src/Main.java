7a57ddf166aca555e944bdf940d7932f37212131
Julian Huber <huber@fim.uni-passau.de>
2023-01-17 21:46:18 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        String pw = "thrift,Test02";
+        String salt = StringGenerator.generateSalt();
+        String pwHash = PasswordHash.hash(pw.toCharArray(), salt);
+        System.out.println(pwHash);
+        System.out.println(salt);
+    }
+}
